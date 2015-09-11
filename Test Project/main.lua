b743c1aa1c0b9e5771833e2100392d46210b9459
Michael Kochell <kochelmj@rose-hulman.edu>
2015-09-11 16:06:00 -0400

    Committing inital test code

--- Test Project/main.lua
+++ Test Project/main.lua
@@ -1,3 +1,9 @@
+function my_super_function( arg1, arg2 ) return arg1 + arg2 end
+
+require('runtests')
+
+
+
 local background = Bitmap.new(Texture.new("field.png"))
 stage:addChild(background)
 
---
+++ Test Project/runtests.lua
@@ -0,0 +1,24 @@
+-- Unit testing starts
+luaunit = require('luaunit')
+
+TestMyStuff = {} --test class
+	function TestMyStuff:testWithNumbers()
+		luaunit.assertTrue(true)
+	end
+
+	function TestMyStuff:testWithRealNumbers()
+		luaunit.assertFalse(true) --expected to fail
+	end
+
+TestMyStuff2 = {} --test class
+	function TestMyStuff2:testWithNumbers()
+		luaunit.assertTrue(false) --expected to fail
+	end
+
+	function TestMyStuff2:testWithRealNumbers()
+		luaunit.assertFalse(false)
+	end
+
+
+
+luaunit.LuaUnit.run()
