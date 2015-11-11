0cafa63e8e9450f25c9a93b0edc2ba0b3d9e049e
Michael Kochell <kochelmj@rose-hulman.edu>
2015-11-11 06:48:37 -0500

    rematch/quit server 2

--- RCRServer/game.py
+++ RCRServer/game.py
@@ -3,28 +3,14 @@ __author__ = 'kochelmj'
 import threading
 import util
 from pprint import pprint
+import staticdata
+import collectgame_algorithms
 
-class Directions:
-    UP = "UpMove"
-    DOWN = "DownMove"
-    LEFT = "LeftMove"
-    RIGHT = "RightMove"
-
-class Actions:
-    _directions = {Directions.UP: (0, 1),
-                   Directions.DOWN: (0, -1),
-                   Directions.RIGHT: (1, 0),
-                   Directions.LEFT: (-1, 0)}
-    _directionsAsList = _directions.items()
-
-    def directionToVector(direction):
-        dx, dy = Actions._directions[direction]
-        return (dx, dy)
-    directionToVector = staticmethod(directionToVector)
 
 class Game(object):
 
     MAXTURNS = 10
+    MAXSCORE = 5
     def __init__(self, p1Thread, gameID, packet):
         p1Thread.index = 0
         self.gameID = gameID
@@ -38,124 +24,33 @@ class Game(object):
         self.gameType = packet.get('gametype')
         self.difficulty = packet.get('difficulty')
         self.ready = [False, False]
+        self.rematchBools = [False, False]
 
 
 
     joinSuccess = \
-    {
-        'type': 'Join Game',
-        'success': True
-    }
-
-    def setup(self):
-        self.threads[1].sendData(self.joinSuccess)
-        gamesetup = \
         {
-            'type': 'Game Setup',
-            'gametype': 'Collect',
-            'gridsize': 10,
-            'celldata':
-            {
-                'goldLocations':
-                [
-                    {
-                        'x': 2,
-                        'y': 2
-                    },
-                    {
-                        'x': 3,
-                        'y': 3
-                    },
-                    {
-                        'x': 5,
-                        'y': 5
-                    },
-                    {
-                        'x': 6,
-                        'y': 6
-                    },
-                    {
-                        'x': 8,
-                        'y': 8
-                    },
-                    {
-                        'x': 9,
-                        'y': 9
-                    },
-                    {
-                        'x': 2,
-                        'y': 9
-                    },
-                    {
-                        'x': 3,
-                        'y': 8
-                    },
-                    {
-                        'x': 5,
-                        'y': 6
-                    },
-                    {
-                        'x': 6,
-                        'y': 5
-                    },
-                    {
-                        'x': 8,
-                        'y': 3
-                    },
-                    {
-                        'x': 9,
-                        'y': 2
-                    },
-
-
-                ],
-                'gemLocations':
-                [
-                    {
-                        'x': 4,
-                        'y': 4
-                    },
-                    {
-                        'x': 7,
-                        'y': 7
-                    },
-                    {
-                        'x': 4,
-                        'y': 7
-                    },
-                    {
-                        'x': 7,
-                        'y': 4
-                    },
-                ],
-                'treasureLocations':
-                [
-                    {
-                        'x': 1,
-                        'y': 10
-                    },
-                    {
-                        'x': 10,
-                        'y': 1
-                    },
-                ],
-                'lepStart':
-                {
-                    'x': 5,
-                    'y': 6
-                }
-            }
+            'type': 'Join Game',
+            'success': True
         }
+
+    def setup(self, initial):
+        self.currentTurn = 0
+        if initial:
+            self.threads[1].sendData(self.joinSuccess)
+
+        gamesetup = staticdata.gamesetup
+
         for client in self.threads:
             client.sendData(gamesetup)
 
         locations = \
-        {
-            'p1': {'x': 1, 'y': 1},
-            'p2': {'x': 10, 'y': 10},
-            'lep': {'x': 5, 'y': 5}
-        }
-        lepMoves = self.calculateLepMoves(locations['p1'], locations['p2'], locations['lep'])
+            {
+                'p1': {'x': 1, 'y': 1},
+                'p2': {'x': 10, 'y': 10},
+                'lep': {'x': 5, 'y': 5}
+            }
+        lepMoves = collectgame_algorithms.calculateLepMoves(self, locations)
 
         with self.lock:
             self.currentMoves['lep'] = lepMoves
@@ -178,10 +73,10 @@ class Game(object):
         p2Thread.index = 1
         self.threads.append(p2Thread)
         playerJoined = \
-        {
-            'type': 'Player Joined',
-            'username': p2Thread.userInfo.get('username')
-        }
+            {
+                'type': 'Player Joined',
+                'username': p2Thread.userInfo.get('username')
+            }
         self.threads[0].sendData(playerJoined)
 
 
@@ -194,22 +89,34 @@ class Game(object):
                 self.finishTurn()
 
 
-    def updateLocations(self, locations):
-        p1Loc = locations.get('p1')
-        p2Loc = locations.get('p2')
-        lepLoc = locations.get('lep')
+    def updateLocations(self, locations, scores):
 
-        lepMoves = self.calculateLepMoves(p1Loc, p2Loc, lepLoc)
+        if self.currentTurn % 2 == 0:
+            newItemLocations = collectgame_algorithms.calculateItemLocations(locations)
+        else:
+            newItemLocations = {'goldLocations': [], 'gemLocations': [], 'treasureLocations': []}
+
+        lepMoves = collectgame_algorithms.calculateLepMoves(self, locations)
 
         with self.lock:
             self.currentMoves['lep'] = lepMoves
 
             if self.checkFinish():
                 self.finishTurn()
+
+        if self.checkEndGame(scores):
+            return
+
         for client in self.threads:
-            client.sendData({'type': 'Update Locations', 'locations': {}})
+            client.sendData({'type': 'Update Locations', 'locations': newItemLocations})
 
-
+    def checkEndGame(self, scores):
+        if scores[0] >= self.MAXSCORE or \
+        scores[1] >= self.MAXSCORE or \
+        self.currentTurn >= self.MAXTURNS:
+            self.endGame(scores)
+            return True
+        return False
 
     def checkFinish(self):
         '''
@@ -221,22 +128,19 @@ class Game(object):
                 return False
         return True
 
-    def finishTurn(self):
-        if self.currentTurn >= self.MAXTURNS:
-            self.endGame()
-            return
+    def finishTurn(self, **kw):
         self.currentTurn = self.currentTurn + 1
 
         packet = \
-        {
-            'type': 'Run Events',
-            'events': \
             {
-                'p1': self.currentMoves[0],
-                'p2': self.currentMoves[1],
-                'lep': self.currentMoves['lep']
+                'type': 'Run Events',
+                'events': \
+                    {
+                        'p1': self.currentMoves[0],
+                        'p2': self.currentMoves[1],
+                        'lep': self.currentMoves['lep']
+                    }
             }
-        }
 
         for client in self.threads:
             client.sendData(packet)
@@ -244,82 +148,46 @@ class Game(object):
         for key in self.currentMoves.keys():
             self.currentMoves[key] = None
 
-    def endGame(self):
-        packet = \
-        {
-            'type': 'End Game',
-            'rematch': False
-        }
+    def endGame(self, scores):
+        packet = {'type': 'Game Over'}
+        if scores[0] == scores[1]:
+            packet['winner'] = 'None'
+        elif scores[0] > scores[1]:
+            packet['winner'] = self.threads[0].userInfo.get('username')
+        else:
+            packet['winner'] = self.threads[1].userInfo.get('username')
+
         for client in self.threads:
             client.sendData(packet)
 
 
-    def calculateLepMoves(self, p1Loc, p2Loc, lepLoc):
-        lepMoves = self.maxMoves + 1
-        visitedStates = set()
-        frontier = util.PriorityQueue()
-        currentState = (lepLoc.get('x'), lepLoc.get('y'))
-        stateCost = 0
-        hVal = self.collectEnemyHeuristic(p1Loc, p2Loc, currentState)
-        p = hVal + stateCost
-        node = (currentState, [], stateCost, hVal)
-        frontier.push(node, p)
-        while(1):
-            if frontier.isEmpty():
-                return []
-            currentState, actions, stateCost, hVal = frontier.pop()
-            if len(actions) == lepMoves:
-                return actions
-            if currentState not in visitedStates:
-                visitedStates.add(currentState)
-                successors = self.getSuccessors(currentState)
-                for successor in successors:
-                    nextState, nextAction, nextCost = successor
-                    cost = stateCost
-                    newActions = list(actions)
-                    newActions.append(nextAction)
-                    cost += stateCost
-                    newHVal = self.collectEnemyHeuristic(p1Loc, p2Loc, nextState)
-                    pVal = newHVal + cost
-                    newNode = (nextState, newActions, cost, newHVal)
-                    frontier.push(newNode, pVal)
-        return []
+
+    def rematch(self, client, packet):
+        choice = packet.get('rematch')
+        if not choice:
+            index = (client.index + 1) // 2
+            self.threads[index].sendData({'type': 'Rematch', 'rematch': False})
+            self.cleanUp()
+            return
+
+        with self.lock:
+            self.rematchBools[client.index] = True
+            if all(self.rematchBools):
+                self.rematchBools = [False, False]
+                self.setup(False)
 
 
-    def collectEnemyHeuristic(self, p1Loc, p2Loc, state):
-        p1 = p1Loc['x'], p1Loc['y']
-        p2 = p2Loc['x'], p2Loc['y']
-        p1Dist = util.manhattanDistance(state, p1)
-        p2Dist = util.manhattanDistance(state, p2)
-        avg = (p2Dist + p1Dist) / 2
-        return 1 / avg
-
-    def getSuccessors(self, state):
-        successors = []
-        x , y = state
-        for action in [Directions.UP, Directions.DOWN, Directions.LEFT, Directions.RIGHT]:
-            dx, dy = Actions.directionToVector(action)
-            newX, newY = int(x + dx), int(y+dy)
-            if not self.isWall(newX, newY):
-                print newX, newY
-                nextState = (newX, newY)
-                cost = self.calculateStateCost(nextState)
-                successors.append((nextState, action, cost))
-        return successors
-
-    def calculateStateCost(self, state):
-        return 1
-
-    def isWall(self, x, y):
-        if x < 1 or y < 1 or x > 10 or y > 10:
-            return True
-        return False
+    def cleanUp(self):
+        '''
+        Here in case we need to clean up any objects
+        '''
+        pass
 
     def refuse(self):
         '''
         Host refused to play client
-        :return:
         '''
         pass
         #TODO: implement kicking out peer
+
 
