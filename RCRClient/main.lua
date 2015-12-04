fdaa89152d78fdddc3536f092c642bbd6069453a
Michael Kochell <kochelmj@rose-hulman.edu>
2015-12-04 17:55:17 -0500

    change transition to crossfade

--- RCRClient/main.lua
+++ RCRClient/main.lua
@@ -6,10 +6,7 @@ local titleScene = gideros.class(Sprite)
 
 local secondScene = gideros.class(Sprite)
 
-function secondScene:init()
-	local logo = Bitmap.new(Texture.new("images/RcrLogo.png"))
-	self:addChild(logo)
-end
+
 
 sceneManager = SceneManager.new({ 
 	["title"] = titleScene,
@@ -20,12 +17,20 @@ function titleScene:init()
 	local titleBackground = Bitmap.new(Texture.new("images/moonbackground.png"))
 	titleBackground:setScale(1.5, 1)
 	local titleClick = Button.new(titleBackground, titleBackground, function() 
-		sceneManager:changeScene("second", 1, SceneManager.flipWithFade, easing.outBack) 
+		sceneManager:changeScene("second", 1, SceneManager.crossfade, easing.outBack) 
 	end)
 	self:addChild(titleClick)
 end
 
+function secondScene:init()
+	local logo = Bitmap.new(Texture.new("images/RcrLogo.png"))
+	local logoClick = Button.new(logo, logo, function() 
+		sceneManager:changeScene("title", 1, SceneManager.crossfade, easing.outBack) 
+	end)
+	self:addChild(logoClick)
+end
+
 stage:addChild(sceneManager)
 
-sceneManager:changeScene("title", 1, SceneManager.flipWithFade, easing.outBack)
+sceneManager:changeScene("title", 1, SceneManager.crossfade, easing.outBack)
 
