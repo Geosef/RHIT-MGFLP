7d335d4283c4ab5f0fd9b207a200c56bca3d6e08
Bill Mader <maderwc@rose-hulman.edu>
2015-12-07 21:24:08 -0500

    Added email input
    Added pop-up text input to the login page. Not quite what we're looking
    for, but it's a start. Will discuss other methods of input at tomorrow's
    meeting.

--- RCRClient/login.lua
+++ RCRClient/login.lua
@@ -3,8 +3,15 @@ login = gideros.class(Sprite)
 function login:init()
 	local titleBackground = Bitmap.new(Texture.new("images/moonbackground.png"))
 	titleBackground:setScale(1.5, 1)
-	local titleClick = Button.new(titleBackground, titleBackground, function() 
+	self:addChild(titleBackground)
+	
+	local emailInputDialog = TextInputDialog.new("Login", 
+		"Enter your email and password",
+		"", "Cancel", "Login")
+	local function onComplete(event)
 		sceneManager:changeScene("splash", 1, SceneManager.crossfade, easing.outBack) 
-	end)
-	self:addChild(titleClick)
+	end
+	
+	emailInputDialog:addEventListener(Event.COMPLETE, onComplete)
+	emailInputDialog:show()	
 end
